f2ff1dc4e0f9cc6358988d19dc0f6408f95463a4
GaelRS <grosas04@alumnos.uaq.mx>
2025-02-26 16:34:52 -0600

    feat:Se añadio el ejercicio de Flatlist

---
+++ flatlist-example/styles/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+    },
+    header: {
+      width: '100%',
+      height: 100,
+      marginTop: 20,
+      backgroundColor: 'blue',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textTitle: {
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
+    body: {
+      padding: 20,
+    },
+    textBody: {
+      fontSize: 20,
+    },
+    flatList: {
+      marginTop: 20,
+      width: '100%',
+      height: '80%',
+      backgroundColor: 'red',
+      
+    },
+    flatListText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white'
+    }
+  });
+export default styles;  
